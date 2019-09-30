bb817f72f1e8e61259dd189797d2d19ed24a7af8
Thibaud <thibaud@MacBook-Pro-de-Thibaud.local>
2019-09-30 16:25:49 +1000

    Improve documentation

--- filter/blackListFilter.js
+++ filter/blackListFilter.js
@@ -1,13 +1,16 @@
 /**
- * Filter that parse the email to check if it contains any forbidden link from a provided black list.
+ * Filter that parses the email to check if it contains any forbidden link from a provided black list.
  * @param email The attacker email
+ * @returns True if the email is accepted, false otherwise
  */
 function blackListFilter(email) {
-    let accept = true;
+    let accept = noFilter(email);
+    
+    if (!accept) return accept;
 
     /**
      * Check if any of the black listed websites are in the email.
-     * If so, return false;
+     * If so, return false
      */
     BLACK_LIST.forEach(blackListedWebsite => {
         if (email.search(blackListedWebsite) != -1) {
--- filter/noFilter.js
+++ filter/noFilter.js
@@ -1,6 +1,7 @@
 /**
  * Naive filter that always return true.
  * @param email The attacker email
+ * @returns True if the email is accepted (always)
  */
 function noFilter(email) {
     return true;
